86985c132d5bb2e31e30713ca5342a0f504274b8
Karaokesnr <ann164628@gmail.com>
2022-01-22 19:46:00 +0300

    a guessing game

---
+++ guessing-game.py
@@ -0,0 +1,12 @@
+print ("GUESSING GAME")
+secret_no = 9
+guess_no = 0
+guess_limit= 3
+while guess_no<guess_limit:
+    guess =int(input ('Make a guess: '))
+    guess_no +=1
+    if guess == secret_no:
+        print ("You won!")
+        break
+else:
+    print("Sorry you lost.")
