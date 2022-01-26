23364b517590b1114f498b6dc31cb82e6a48e846
Karaokesnr <ann164628@gmail.com>
2022-01-26 14:21:03 +0300

    Update guessing-game.py

--- guessing-game.py
+++ guessing-game.py
@@ -1,15 +1,15 @@
-print ("""
-A  GUESSING GAME
-Enter a single digit number to play
-""")
-secret_no = 9
+print("A simple guessing game")
+secret_no = 5
 guess_no = 0
-guess_limit= 3
-while guess_no<guess_limit:
-    guess =int(input ('Make a guess: '))
+guess_lim = 3
+
+while guess_no < guess_lim:
+    guess = int(input("Make a guess: "))
     guess_no +=1
     if guess == secret_no:
-        print ("You won!")
+        print("You won!")
         break
+    elif guess_no < guess_lim:
+        print ("Sorry, try again")
 else:
-    print("Sorry you lost.")
+    print("You snooze you loose!")
